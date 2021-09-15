55ab01fc34ddf8d178bc41c6a21fceabf47ff71f
Aniket Bhandarkar <akb501@nyu.edu>
2021-09-15 18:04:53 -0400

    Add model and corresponding migration to store tasks

--- server/todo/models.py
+++ server/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+   title = models.CharField(max_length=128)
+   description = models.TextField()
+   completed = models.BooleanField(default=False)
+   created_on = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+   modified_on = models.DateTimeField(null=True, blank=True)
+
+   def __str__(self):
+     return self.title
