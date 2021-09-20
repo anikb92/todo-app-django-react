de86b4c4ad7a45b8b516266d123f087993058a93
Aniket Bhandarkar <akb501@nyu.edu>
2021-09-20 18:17:46 -0400

    Update base component for the frontend

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tasks = [
+  {
+    id: 1,
+    title: "Backend",
+    description: "Create processing module plus backend",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Frontend",
+    description: "Create frontend using React",
+    completed: true
+  },
+];
+
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {tasks};
+    };
+
+    render() {
+      return (
+        <main className="content">
+            <div className="row">
+              <div className="col-md-6 col-sm-10 mx-auto p-0">
+                <div className="card p-3">
+                  <ul className="list-group list-group-flush">
+                  {this.state.tasks.map(item => (
+                  <div>
+                    <h1>{item.title}</h1>
+                    <span>{item.description}</span>
+                  </div>
+                  ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+        </main>
+      )
+    }
 }
 
 export default App;
