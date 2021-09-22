da50fa8fc243ab23a65b270baba2d09b7aa698a2
Aniket Bhandarkar <akb501@nyu.edu>
2021-09-22 00:26:49 -0400

    Add code in frontend to fetch tasks using API

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,46 +1,68 @@
 import React, { Component } from "react"
 
-const tasks = [
-  {
-    id: 1,
-    title: "Backend",
-    description: "Create processing module plus backend",
-    completed: true
-  },
-  {
-    id: 2,
-    title: "Frontend",
-    description: "Create frontend using React",
-    completed: true
-  },
-];
-
 class App extends Component {
     constructor(props) {
-      super(props);
-      this.state = {tasks};
+        super(props);
+        this.state = {
+          currTask: {
+            title: "",
+            description: "",
+            completed: false
+          },
+          tasks: [],
+          showCompleted: false,
+        };
+    }
+
+    async componentDidMount() {
+      try {
+        const response = await fetch('');
+        const tasks = await response.json();
+        this.setState({
+          tasks
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    renderTasks = () => {
+      const { showCompleted } = this.state;
+      const newTasks = this.state.tasks.filter(
+        task => task.completed === showCompleted
+      );
+      return newTasks.map(task => (
+        <li
+          key={task.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span
+            className={`task-title mr-2 ${
+              this.state.showCompleted ? "completed-task" : ""
+            }`}
+            title={task.description}
+            >
+              {task.title}
+            </span>
+        </li>
+      ));
     };
 
     render() {
       return (
         <main className="content">
-            <div className="row">
-              <div className="col-md-6 col-sm-10 mx-auto p-0">
-                <div className="card p-3">
-                  <ul className="list-group list-group-flush">
-                  {this.state.tasks.map(item => (
-                  <div>
-                    <h1>{item.title}</h1>
-                    <span>{item.description}</span>
-                  </div>
-                  ))}
-                  </ul>
-                </div>
-              </div>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <ul className="list-group list-group-flush">
+                {this.renderTasks()}
+              </ul>
             </div>
-        </main>
+          </div>
+        </div>
+      </main>
       )
     }
-}
+  }
 
 export default App;
